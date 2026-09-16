76003d2496b988a52fdcf44a36e281b7ba9d118f
54mjbmz777-cmd <54mjbmz777@privaterelay.appleid.com>
2026-09-15 23:41:16 -0400

    Add fancySearch command

---
+++ fancysearch.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+
+const filename = process.argv[2];
+const searchWord = process.argv[3];
+const numberOfLines = Number(process.argv[4]);
+
+if (!filename || !searchWord || !numberOfLines) {
+  console.log("Usage: node fancysearch.js <filename> <searchWord> <numberOfLines>");
+  process.exit();
+}
+
+if (!fs.existsSync(filename)) {
+  console.log("Error: File could not be found.");
+  process.exit();
+}
+
+const fileContents = fs.readFileSync(filename, "utf8");
+const lines = fileContents.split("\n");
+
+const matchingLines = lines.filter((line) =>
+  line.toLowerCase().includes(searchWord.toLowerCase())
+);
+
+if (matchingLines.length === 0) {
+  console.log("No matching lines found.");
+} else {
+  const firstMatches = matchingLines.slice(0, numberOfLines);
+  console.log(firstMatches.join("\n"));
+}
